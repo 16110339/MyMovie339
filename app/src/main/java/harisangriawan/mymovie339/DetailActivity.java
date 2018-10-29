c5776afa2c53c78a04d15449564009a6218edb2f
HarisAngriawan <39801304+16110339@users.noreply.github.com>
2018-10-29 22:26:44 +0700

    Makasih

--- app/src/main/java/harisangriawan/mymovie339/DetailActivity.java
+++ app/src/main/java/harisangriawan/mymovie339/DetailActivity.java
@@ -1,19 +1,30 @@
 package harisangriawan.mymovie339;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
 
+import harisangriawan.mymovie339.adapter.MovieAdapter;
+import harisangriawan.mymovie339.api.ApiClient;
+import harisangriawan.mymovie339.api.ApiInterface;
+import harisangriawan.mymovie339.model.Movie;
 import harisangriawan.mymovie339.model.Result;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
 
     ImageView poster;
-    TextView Judul, Rating, Release, Overview;
+    TextView Judul, Rating, Release, Bahasa, Overview;
 
     Result result;
 
@@ -25,6 +36,7 @@ public class DetailActivity extends AppCompatActivity {
         Judul = (TextView)findViewById(R.id.judul);
         Rating = (TextView)findViewById(R.id.rating);
         Release = (TextView)findViewById(R.id.release);
+        Bahasa = (TextView)findViewById(R.id.bahasa);
         Overview = (TextView)findViewById(R.id.overview);
 
         result = new GsonBuilder()
@@ -37,7 +49,15 @@ public class DetailActivity extends AppCompatActivity {
         Judul.setText(result.getTitle());
         Rating.setText(Double.toString(result.getVoteAverage()));
         Release.setText(result.getReleaseDate());
+        Bahasa.setText(result.getOriginalLanguage());
         Overview.setText(result.getOverview());
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.list_sort_setting, menu);
+
+        return true;
+    }
+
 }
