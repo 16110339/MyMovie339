c18f07ebc54ebf95206edd518851da264d88f56b
HarisAngriawan <39801304+16110339@users.noreply.github.com>
2018-10-29 19:43:48 +0700

    bye

--- app/src/main/java/harisangriawan/mymovie339/MainActivity.java
+++ app/src/main/java/harisangriawan/mymovie339/MainActivity.java
@@ -1,11 +1,16 @@
 package harisangriawan.mymovie339;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -49,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
             movieLoad("popular");
         }else if (id == R.id.top_rated){
             movieLoad("top_rated");
+        }else if (id ==R.id.setting){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -77,4 +85,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
